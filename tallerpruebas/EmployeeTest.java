fadabce15394c761486d7af94f492c6d687910d9
melina-maria <melina_maria@hotmail.es>
2021-01-20 23:07:36 -0500

    Pruebas de primer Metodo para Manager y Supervisor

--- tallerpruebas/EmployeeTest.java
+++ tallerpruebas/EmployeeTest.java
@@ -3,6 +3,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 public class EmployeeTest {
+	
+	
+	/*
+	 * Prueba que hace Test a metodo cs para calcular el salario de Manager
+	 * Salario=100
+	 * Porcentaje de Bonus = 50
+	 * Currency USD
+	 * 
+	 */
+	
+	@Test
+	public void csManagerTest() {
+		
+		Employee empleado= new Employee((float)100,"USD",(float)50,EmployeeType.Manager);
+		float salary= empleado.cs();
+		assertEquals((float) 181.83334,(float)((float)((float)(100)+((float)(50)*0.35F))+(float)(386.0)/12*2));
+		
+		
+	}
+	/*
+	 * Prueba que hace Test a metodo cs para calcular el salario de Supervisor
+	 * Salario=100
+	 * Porcentaje de Bonus = 50
+	 * Currency USD
+	 * 
+	 */
+	
+	@Test
+	public void csSupervisorTest() {
+		
+		Employee empleado= new Employee((float)100,"USD",(float)50,EmployeeType.Supervisor);
+		float salary= empleado.cs();
+		assertEquals((float) 199.33334,(float)((float)((float)(100)+((float)(50)*0.7F))+(float)(386.0)/12*2));
+		
+		
+	}
+	
+	
 	/*Se prueba que el valor arrojado por el metodo de la clase employee cuando el tipo de empleado sea Worker, sea el correcto
 	 * 
 	 */
